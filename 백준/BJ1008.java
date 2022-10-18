395f1b9c4f62922b30237112a500fa3ac3e4a831
NamYoung Kim <095201a@naver.com>
2022-10-18 14:23:38 +0900

    feat : A/B

---
+++ 백준/BJ1008.java
@@ -0,0 +1,23 @@
+package 백준;
+
+import java.util.Scanner;
+
+public class BJ1008 {
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double a = getNumber();
+        double b = getNumber();
+
+        System.out.println(divide(a, b));
+    }
+
+    protected static double divide(double a, double b) {
+        return a / b;
+    }
+
+    protected static double getNumber() {
+        return sc.nextDouble();
+    }
+}
