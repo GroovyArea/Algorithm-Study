06d3b4d26565b3f2780d964115b09bca7a35e5b6
NamYoung Kim <095201a@naver.com>
2022-10-18 14:34:47 +0900

    feat : 두 수 비교하기

---
+++ 백준/BJ1330.java
@@ -0,0 +1,28 @@
+package 백준;
+
+import java.util.Scanner;
+
+public class BJ1330 {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double a = getNumber();
+        double b = getNumber();
+
+        System.out.println(compareBigAndSmall(a, b));
+    }
+
+    protected static double getNumber() {
+        return SCANNER.nextDouble();
+    }
+
+    protected static String compareBigAndSmall(double a, double b) {
+        if (a > b) {
+            return ">";
+        } else if (a < b) {
+            return "<";
+        } else return "==";
+    }
+
+}
