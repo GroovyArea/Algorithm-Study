8f7942228e24c5890c95320b55fbe5ce11757179
NamYoung Kim <095201a@naver.com>
2022-10-24 15:20:54 +0900

    feat : 단어 정렬 (문자열, 정렬)

---
+++ 백준/Class2/BJ1181.java
@@ -0,0 +1,41 @@
+package 백준.Class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BJ1181 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int inputNumber = Integer.parseInt(br.readLine());
+
+        String[] words = new String[inputNumber];
+
+        for (int i = 0; i < inputNumber; i++) {
+            words[i] = br.readLine();
+        }
+
+        Arrays.sort(words, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                } else {
+                    return o1.length() - o2.length();
+                }
+            }
+        });
+
+        System.out.println(words[0]);
+
+        for (int i = 1; i < inputNumber; i++) {
+            if (!words[i].equals(words[i - 1])) {
+                System.out.println(words[i]);
+            }
+        }
+    }
+}
